24da2ab15651f240521223e06d55859c7ced0c25
Misha Goryn <mgoryn68@gmail.com>
2024-12-01 15:38:45 +0200

    - add new blank line to ".gitignore" file - add new migrations - add & update models.py

--- cinema/models.py
+++ cinema/models.py
@@ -4,7 +4,33 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    duration = models.IntegerField()
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    actors = models.ManyToManyField("Actor", related_name="movies")
+    genres = models.ManyToManyField("Genre", related_name="movies")
 
     def __str__(self):
         return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"First_name: {self.first_name} | Last_name: {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name} {self.rows} {self.seats_in_row}"
